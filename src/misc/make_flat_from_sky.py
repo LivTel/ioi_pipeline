21c053ae1639e1190ce5fdc725c673ba84f65d3d
Rob Barnsley <R.M.Barnsley@ljmu.ac.uk>
2015-04-09 15:10:08 +0100

    Robustified make_flat

--- src/misc/make_flat_from_sky.py
+++ src/misc/make_flat_from_sky.py
@@ -6,6 +6,7 @@ import argparse
 import logging
 import os
 import numpy as np
+import scipy.stats as sps
 
 sys.path.append("../")
 from utility import read_FITS_file, write_FITS_file
@@ -18,7 +19,8 @@ if __name__ == "__main__":
     parser.add_argument('--fits', action='store_true', dest='fits', help='make flat and bad pixel map FITS files')
     parser.add_argument('--fl', action='store_true', dest='flip', help='flip array on write?')
     parser.add_argument('--hdu', action='store', dest='hdu', default=0, type=int, help='hdu index to use')
-    parser.add_argument('--o', action='store_true', dest='clobber', default=False, help='clobber existing files on write?')
+    parser.add_argument('--o', action='store_true', dest='clobber', help='clobber existing files on write?')
+    parser.add_argument('--sig', action='store', dest='sigclip', default=10, type=int, help='sigma to use for clipping dataset prior to mean calculation')
     
     args = parser.parse_args()
     params = {
@@ -28,7 +30,8 @@ if __name__ == "__main__":
         'fits' : bool(args.fits),
         'flip' : bool(args.flip),
         'hdu' : int(args.hdu),
-        'clobber' : bool(args.clobber)
+        'clobber' : bool(args.clobber),
+        'sigclip' : int(args.sigclip)
     }    
     
     # console logging
@@ -54,18 +57,24 @@ if __name__ == "__main__":
     for i in params['paths']:
         if os.path.exists(i):
             logger.info("reading file " + i + " at HDU " + str(params['hdu']))
-            data, hdr = read_FITS_file(i)
-            data_mean = np.nanmean(data[params['hdu']])
-            logger.info("mean of data pre-normalisation is " + str(data_mean))
+            data, hdr = read_FITS_file(i, hdu=params['hdu'])
+            logger.info("converting NaNs to numbers")
+            data_nonan = np.nan_to_num(data)
+            logger.info("clipping data with sigma of " + str(params['sigclip']))
+            data_nonan_sigclip = sps.sigmaclip(data_nonan, low=params['sigclip'], high=params['sigclip'])[0]
+            data_nonan_sigclip_mean = np.mean(data_nonan_sigclip)
+            logger.info("mean of data pre-normalisation is " + str(data_nonan_sigclip_mean))
             logger.info("normalising data") 
-            data_cor = data/data_mean
-            logger.info("mean of data post-normalisation is " + str(np.nanmean(data_cor))) 
+            data_cor = data_nonan/data_nonan_sigclip_mean
+            logger.info("mean of data post-normalisation is " + str(np.mean(data_cor))) 
             datas_cor.append(data_cor)
         else:
             logger.warning("path " + i + " does not exist. skipping.")        
-    
-    # make flat and set "bad pixels" to 1
-    flat = np.nanmean(datas_cor, axis=0) 
+
+    # create master flat if more than one file specified
+    flat = np.mean(datas_cor, axis=0) 
+        
+    # make flat and set "bad pixels" to 1        
     bad_array   = np.where(flat < params['badThresh'])   # establish bad pixels
     flat[bad_array] = 1 
     if params['fits']:
@@ -88,6 +97,7 @@ if __name__ == "__main__":
     if params['fits']:   
         if params['clobber']:
             if os.path.exists("flat_bad.fits"):
+                os.remove("flat_bad.fits")
                 logger.info("clobber set. removed flat_bad.fits file") 
         logger.info("writing bad pixel map")    
         if params['flip']:

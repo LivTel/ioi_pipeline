7b5d1de1453852ac1244e2d29a8a715642df221d
Rob Barnsley <R.M.Barnsley@ljmu.ac.uk>
2015-05-05 16:14:38 +0100

    Added statsmodels.

---
+++ src/statsmodels/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.7.0'
+version = '0.7.0'
+full_version = '0.7.0.dev0+a1f53b0'
+git_revision = 'a1f53b04f605341647a02ba452d8b2247c50c9a7'
+release = False
+
+if not release:
+    version = full_version
